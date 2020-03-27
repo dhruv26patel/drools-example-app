74d8ee689cc1eea79f65af1ab4641943b3c7b8dc
Dhuruv Patel <dhruv26patel@gmail.com>
2020-03-27 11:24:03 -0500

    refactored DroolsConfig.java

--- drools-exmaple/src/main/java/com/example/droolsexmaple/config/DroolsConfig.java
+++ drools-exmaple/src/main/java/com/example/droolsexmaple/config/DroolsConfig.java
@@ -4,37 +4,28 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.KieModule;
-import org.kie.api.builder.KieRepository;
 import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
 import org.kie.internal.io.ResourceFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DroolsConfig {
 
-    private KieServices kieServices = KieServices.Factory.get();
-
-    private KieFileSystem getKieFileSystem() {
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newClassPathResource("pets.drl"));
-        return kieFileSystem;
-    }
+    @Value("${drools.fileName}")
+    private String drlFile;
 
     @Bean
     public KieContainer getKieContainer() {
-        KieRepository kieRepository = kieServices.getRepository();
-        kieRepository.addKieModule(() -> kieRepository.getDefaultReleaseId());
-        KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
-        kb.buildAll();
-        KieModule kieModule = kb.getKieModule();
-        KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
-        return kContainer;
-    }
+        KieServices kieServices = KieServices.Factory.get();
 
-    @Bean
-    public KieSession getKieSession() {
-        return getKieContainer().newKieSession();
+        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+        kieFileSystem.write(ResourceFactory.newClassPathResource(drlFile));
+        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+        kieBuilder.buildAll();
+        KieModule kieModule = kieBuilder.getKieModule();
+
+        return kieServices.newKieContainer(kieModule.getReleaseId());
     }
 }
